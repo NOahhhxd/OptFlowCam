corrected googleEarth calculation

src/optFlowCam/interpolation.py
@@ -22,7 +22,7 @@ from .objects.GoogleEarthFile import GoogleEarthStudio
 def export_geoposition_data(cams, num_frames, metric, file_path, earth_center=Vector((0, 0, 0)), earth_radius=10):
     if num_frames != len(cams):
         num_frames = len(cams)
-    studio = GoogleEarthStudio(num_frames, metric, 24, earth_center, earth_radius)
+    studio = GoogleEarthStudio(num_frames, metric, 30, earth_center, earth_radius)
     for idx, cam in enumerate(cams):
         studio.append_frame(cam, idx)
 
@@ -71,12 +71,13 @@ def shortest_path_of(path, min_distance=2):
         if not longest_line:
             return path
         else:
-            del path[longest_line[0] + 1:longest_line[1]]
+            del path[longest_line[0] + 1:longest_line[1]]       
         if len(path) < 3:
             return path
 
 
 def lift_up(path, obj, factor=0.00001):
+
     result = []
     mat_inv = obj.matrix_world.inverted()
     mat = obj.matrix_world
@@ -88,6 +89,9 @@ def lift_up(path, obj, factor=0.00001):
 
 class InterpolateGeodesic:
     def __init__(self, start_cam, end_cam, focal, greedy_method=False, min_greedy_point_difference=2):
+        """
+        Initialized attributes and calculates the geodesic between start and end cam
+        """
         start_eyepoint = start_cam["position"]
         start_view_direction = start_cam["view"]
         end_eyepoint = end_cam["position"]
@@ -107,56 +111,44 @@ class InterpolateGeodesic:
         if obj and obj == obj2 and obj.type == "MESH":
             self.mesh = obj.evaluated_get(vl.depsgraph).to_mesh()
             self.obj = obj
-            # [(bpy.data.objects['Sphere'].matrix_world @ i.co) for i in bpy.data.objects['Sphere'].data.vertices]
         else:
             raise ValueError("Start-/Endkamera müssen auf dasselbe Objekt gerichtet sein")
         self.distance = None
         self.path = []
-        faces = [i.vertices for i in self.mesh.polygons]
-        start_face = faces.pop(start_face_idx)
-        end_face = faces.pop(end_face_idx)
-        faces += new_face(start_face, len(self.mesh.vertices))
-        faces += new_face(end_face, len(self.mesh.vertices) + 1)
-        self.geodesic_calc = geodesic.PyGeodesicAlgorithmExact(
-            [self.obj.matrix_world @ i.co for i in self.mesh.vertices] + [start_loc, end_loc], faces)
-        # [i for i in bpy.data.meshes["Icosphere"].polygons[0].vertices] )# self.mesh.polygons)
-        self.calculate(start_cam, end_cam, 0, 1, len(self.mesh.vertices), focal)
+        if start_face_idx == end_face_idx:
+            self.path = [(start_loc, 0), (end_loc, 1)]
+            self.distance = (start_loc - end_loc).length
+        else:
+            faces = [i.vertices for i in self.mesh.polygons if i.index != start_face_idx and i.index != end_face_idx]
 
-    def calculate(self, start, end, start_t, end_t, idx, focal):
+            start_face = self.mesh.polygons[start_face_idx].vertices  # faces.pop(start_face_idx)
+            end_face = self.mesh.polygons[end_face_idx].vertices  # faces.pop(end_face_idx)
+            faces += new_face(start_face, len(self.mesh.vertices))
+            faces += new_face(end_face, len(self.mesh.vertices) + 1)
+            self.geodesic_calc = geodesic.PyGeodesicAlgorithmExact(
+                [self.obj.matrix_world @ i.co for i in self.mesh.vertices] + [start_loc, end_loc], faces)
+            # [i for i in bpy.data.meshes["Icosphere"].polygons[0].vertices] )# self.mesh.polygons)
+            self.calculate(0, 1, len(self.mesh.vertices))
+
+    def calculate(self, start_t, end_t, idx):
         """
-        cam = {
-        "position": pos.tolist(),
-        "view": view.tolist(),
-        "up": up.tolist(),
-        "frustum_scale": scale,
-        "focal": focal
-        }
+        Calculate the geodesic path between start and end and combine path with time.
         """
         assert start_t < end_t
-        """
-        start_eyepoint = start["position"]
-        start_view_direction = start["view"]
-        end_eyepoint = end["position"]
-        end_view_direction = end["view"]
-        _, start_face_idx, start_loc = self.raycast(start_eyepoint, start_view_direction)
-        start_idx = self.get_idx(start_face_idx, start_loc)
-        _, end_face_idx, end_loc = self.raycast(end_eyepoint, end_view_direction)
-        end_idx = self.get_idx(end_face_idx, end_loc)
-        path = self.find_geodesic_path_between(start_idx, end_idx)
-        """
-        ### Idee: Pfad durch BSpline der Punkte interpolieren lassen
+
         path = self.find_geodesic_path_between(idx, idx + 1)
-        print(f"Länge des {'modifizierten' if self.greedy_geodesic else ''} Pfads: {len(path)}")
+        print(f"Length of {'modified' if self.greedy_geodesic else ''} path: {len(path)}")
         self.mix_path_with_time(path, start_t, end_t)
-        # self.mix_advanced_path_with_time(path, start, start_t, end, end_t, focal)
 
     def get_distance(self):
         return self.distance
 
     def interpolate(self, t):
-        print(t)
-        # assert self.path[0][1] <= t <= self.path[-1][1] , f"{t} is not in {self.path[0][1]} - {self.path[-1][1]}"
-        idx = self.find_t_idx(t)  #
+        """
+        Interpolates a point of the geodesic path by t (0 <= t <= 1)
+        """
+        # structure of self.path[i] = (coordinates, time)
+        idx = self.find_t_idx(t)
         idx = min(max(0, idx), len(self.path) - 1)
         next_element = self.path[idx]
         next_t = next_element[1]
@@ -168,7 +160,6 @@ class InterpolateGeodesic:
             return (1 - t_part) * last_element[0] + t_part * next_element[0]
         else:
             return next_element[0]
-        # return last_element[0] + (next_element[1]-last_element[0])*((t-last_t) / t_diff)
 
     def raycast(self, start, direction) -> tuple[Any, Any, Any]:
         context = bpy.context
@@ -179,18 +170,8 @@ class InterpolateGeodesic:
         assert hit, "Cameras must be pointed at an object"
         return obj_0, face_idx, loc
 
-    def get_idx(self, face_idx, loc):
-        faces = self.mesh.polygons[face_idx]
-        nearest_vert = None
-        for v in faces.vertices:
-            vert = self.mesh.vertices[v]
-            if not nearest_vert or nearest_vert[0] > (vert.co - loc).length:
-                nearest_vert = ((vert.co - loc).length, vert)
-        return nearest_vert[1].index
-
     def find_geodesic_path_between(self, start_idx, end_idx):
         distance, path = self.geodesic_calc.geodesicDistance(end_idx, start_idx)
-        # self.distance = distance
         print("From", start_idx)
         print("to", end_idx)
         print("Distance", distance)
@@ -204,7 +185,6 @@ class InterpolateGeodesic:
                 distance += np.linalg.norm(path[i] - path[i - 1])
 
         self.distance = distance
-        # print(path)
         return path
 
     def mix_path_with_time(self, path, start_t, end_t):
@@ -214,33 +194,11 @@ class InterpolateGeodesic:
             current_dist = np.linalg.norm(path[idx] - path[idx - 1])
             self.path.append((path[idx], self.path[idx - 1][1] + t_diff * current_dist / self.distance))
         self.path.append((path[-1], end_t))
-        # print(self.path)
-        # self.path = list(zip(path, np.linspace(start_t, end_t, len(path))))
 
     def find_t_idx(self, t):
         return bisect.bisect_left(self.path, t, key=lambda x: x[1])
 
-    def mix_advanced_path_with_time(self, path, start, start_t, end, end_t, focal):
-        # look1 = pos1 + s1 * focal * view1
-        pos1, view1, up1, right1, s1 = unpack_camera(start)
-        look1 = pos1 + s1 * focal * view1
-        start_diff = np.linalg.norm(path[0] - look1)
-        pos2, view2, up2, right2, s2 = unpack_camera(end)
-        look2 = pos2 + s2 * focal * view2
-        end_diff = np.linalg.norm(path[-1] - look2)
-
-        self.distance = self.distance + start_diff + end_diff
-        start_part = start_diff / self.distance
-        end_part = end_diff / self.distance
-        t_after_start_part = start_t + (end_t - start_t) * start_part
-        t_before_end_part = end_t - (end_t - start_t) * end_part
-        self.path = list(zip(np.vstack((look1, path, look2)),
-                             [start_t] + list(np.linspace(t_after_start_part, t_before_end_part, len(path))) + [end_t]))
-        # print(self.path)
-
     def update_path_object(self, path_object):
-        # from ..objects.path_geometry import add_path_object, update_path
-        # path_object = add_path_object(2, collection_name, "geodesic_path")
         update_path(path_object, [{"position": i[0]} for i in self.path])
 
 
@@ -619,7 +577,7 @@ def interpolate_simple(start: dict, end: dict,
         print("Exporting data...")
         earth = kwargs["move_around_object"]
         earth_position = earth.location
-        earth_radius = (earth.matrix_world@(earth.data.vertices[0].co-earth.location)).length
+        earth_radius = (earth.matrix_world @ (earth.data.vertices[0].co - earth.location)).length
         export_geoposition_data(cams, n, metric, kwargs["file_path"], earth_center=earth_position,
                                 earth_radius=earth_radius)  # [np.array(cam["position"]) for cam in cams])
         print("Exporting finished")

src/optFlowCam/objects/GoogleEarthFile.py
@@ -1,7 +1,7 @@
 import math, json, numpy as np
 from ..math import make_lookAt_matrix
 from mathutils import Vector, Matrix
-from math import sin,cos,atan2,acos,asin
+from math import sin, cos, atan2, acos, asin
 
 template = """{
   "modelVersion": 18,
@@ -241,8 +241,7 @@ template = """{
 }"""
 
 
-def get_height(position, center, radius):
-    distance = (position - center).length
+def get_height(distance, radius):
     """
     radius/distance = 6371_000 / real_distance
     ======>
@@ -298,63 +297,6 @@ def extract_2d_position(position, earth_center, earth_radius):
 
 def project_point_into_ebene(point, vec, normal):
     return point + (normal @ (vec - point)) * normal
-
-
-def theiselsIdee(pos, dir, earth_center, earth_radius):
-    """
-    1. Schnittpunkt der Sichtachse auf der Erde finden
-    hh := Multiply( Transpose(E+t*A),E+t*A)-r^2;
-
-    #solve(hh=0,t);
-
-    dd :=
-    -Multiply( Transpose(A),E)
-    + sqrt(
-    + r^2*Multiply( Transpose(A),A)
-    -Multiply( Transpose(A&xE),A&xE)
-    );
-    simplify(eval(hh,t=dd));
-
-    M := simplify(E+dd*A);
-
-    2. Spiegel Matrix aufstellen
-    MM_mirror := Matrix([
-    [-1,0,0],
-    [0,1,0],
-    [0,0,-1]]);
-
-###
-    3. Skalierung berechnen
-    s_pio := -dd/f;
-    M_pio := M;
-    RM_pio := Multiply( RM, MM_mirror);
-
-###
-
-A_pio := Vector([ RM_pio[1,3],RM_pio[2,3],RM_pio[3,3] ]);
-E_pio := M_pio - f*s_pio*A_pio;
-
-Longitude_pio := simplify(arctan( E_pio[2],E_pio[1]));
-Latitude_pio  := arctan( E_pio[3] , E_pio[1]/cos(Longitude_pio) );
-Altitude_pio := sqrt(Multiply( Transpose(E_pio),E_pio)) - r;
-
-RMO_pio := Matrix(3, 3, [
-[-sin(Longitude_pio), -sin(Latitude_pio)*cos(Longitude_pio), cos(Longitude_pio)*cos(Latitude_pio)],
-[cos(Longitude_pio), -sin(Latitude_pio)*sin(Longitude_pio), sin(Longitude_pio)*cos(Latitude_pio)],
-[0, cos(Latitude_pio), sin(Latitude_pio)]]);
-
-
-RM1_pio := Multiply( Transpose(RMO_pio) , Multiply( RM_pio, MM_mirror));
-== Weltmatrix ???
-
-#Tilt_pio := arccos(RM1_pio[3,3]);
-Tilt_pio := arctan( sqrt(RM1_pio[1,3]^2 + RM1_pio[2,3]^2) , RM1_pio[3,3] );
-Roll_pio := arctan( -RM1[3,1],RM1[3,2]);
-Pan_pio := arctan(-RM1[1,3],-RM1[2,3]);
-
-
-    """
-
 
 def camMatrixByPosition(lat, long, alt, pan, tilt, roll, r=6371, focal=2):
     from math import sin, cos, radians
@@ -444,12 +386,12 @@ def camMatrixByPosition(lat, long, alt, pan, tilt, roll, r=6371, focal=2):
     RM1 = ((RM_Pan @ RM_Tilt) @ RM_Roll)
     #
     RM = (RM0 @ RM1)
-    #print(np.cross(RM[:, 1], RM[:, 2]))
+    # print(np.cross(RM[:, 1], RM[:, 2]))
     up = Vector(RM[:, 1])
     forward = -Vector(RM[:, 2])
-    #print(RM)
+    # print(RM)
     # return RM_pio, E
-    return make_lookAt_matrix(E, forward, up)# , E, forward, up
+    return make_lookAt_matrix(E, forward, up)  # , E, forward, up
     # mat,pos = camMatrixByPosition(0,45,20,20,11,23,2,2)
     # mat= camMatrixByPosition(0,0,20,180,15,180,2,2)
 
@@ -467,16 +409,10 @@ def extract_rotation(cam, earth_position=np.array((0, 0, 0)), earth_radius=10):
             "focal": focal
         }
         """
-    # forward = -forward
-    # forward = -np.array(cam["view"])
     forward = -np.array(cam["view"])
     up = np.array(cam["up"])
     pos = np.array(cam["position"]) - earth_position
-    # RIGHT = np.cross(up, forward)
-    #
-    # np.array([np.cross(RM[:, 1], RM[:, 2]), RM[:, 1], RM[:, 2]]).T
     RM = np.array([np.cross(up, forward), up, forward]).T
-    print(RM)
     E_pio = pos
     """
     Longitude_pio := simplify(arctan( E_pio[2],E_pio[1]));
@@ -488,18 +424,16 @@ def extract_rotation(cam, earth_position=np.array((0, 0, 0)), earth_radius=10):
     [cos(Longitude_pio), -sin(Latitude_pio)*sin(Longitude_pio), sin(Longitude_pio)*cos(Latitude_pio)], 
     [0, cos(Latitude_pio), sin(Latitude_pio)]]);
     
-    
     RM1_pio := Multiply( Transpose(RMO_pio) , Multiply( RM_pio, MM_mirror));
     
-    
-    #Tilt_pio := arccos(RM1_pio[3,3]);
     Tilt_pio := arctan( sqrt(RM1_pio[1,3]^2 + RM1_pio[2,3]^2) , RM1_pio[3,3] );
     Roll_pio := arctan( -RM1[3,1],RM1[3,2]);
     Pan_pio := arctan(-RM1[1,3],-RM1[2,3]);
     """
     longitude = math.atan2(E_pio[1], E_pio[0])
     latitude = math.atan2(E_pio[2], E_pio[0] / cos(longitude))
-    altitude = Vector(E_pio).length - earth_radius  # for resulting file ist must be scaled to earth scale
+    altitude = Vector(E_pio).length  # - earth_radius  # for resulting file ist must be scaled to earth scale
+    altitude = get_height(altitude, earth_radius)
     RMO_pio = np.array([
         [-sin(longitude), -sin(latitude) * cos(longitude), cos(longitude) * cos(latitude)],
         [cos(longitude), -sin(latitude) * sin(longitude), sin(longitude) * cos(latitude)],
@@ -508,10 +442,12 @@ def extract_rotation(cam, earth_position=np.array((0, 0, 0)), earth_radius=10):
     RM1_pio = RMO_pio.T @ RM
     # RM = (RM0 @ RM1)
     # => RM1 = RM0 ^ -1 @ RM = RM0^T @ RM
-    Tilt_pio = math.atan2(math.sqrt(RM1_pio[0, 2] ** 2 + RM1_pio[1, 2] ** 2), RM1_pio[2, 2])
+    Tilt_pio = ((math.atan2(math.sqrt(RM1_pio[0, 2] ** 2 + RM1_pio[1, 2] ** 2), RM1_pio[2, 2]) + 2 * math.pi)
+                % (2 * math.pi))
     Roll_pio = math.atan2(-RM1_pio[2, 0], RM1_pio[2, 1])
-    Pan_pio = math.atan2(-RM1_pio[0, 2], -RM1_pio[1, 2])
-    return Pan_pio, Tilt_pio, Roll_pio
+    Pan_pio = (math.atan2(-RM1_pio[0, 2], -RM1_pio[1, 2]) + 2 * math.pi) % (2 * math.pi)
+    return longitude, latitude, altitude, Pan_pio, Tilt_pio, Roll_pio
+    # math.degrees(longitude), math.degrees(latitude), altitude, math.degrees(Pan_pio), math.degrees(Tilt_pio), math.degrees(Roll_pio)
 
 
 def extract_data_from_cam(cam, earth_center, earth_radius):
@@ -525,8 +461,8 @@ def extract_data_from_cam(cam, earth_center, earth_radius):
     }
     """
     position = Vector(cam["position"])
-    long, lat, height = extract_2d_position(position, earth_center, earth_radius)
-    rotX, rotY, rotZ = extract_rotation(cam, earth_center, earth_radius)
+    # long, lat, height = extract_2d_position(position, earth_center, earth_radius)
+    long, lat, height, rotX, rotY, rotZ = extract_rotation(cam, earth_center, earth_radius)
     return long, lat, height, rotX, rotY, rotZ
 
 
@@ -535,7 +471,7 @@ def get_relative_value(value, min_value, max_value):
 
 
 class GoogleEarthStudio:
-    def __init__(self, frames=100, name="GoogleEarth", frame_rate=24, earth_center=Vector([0, 0, 0]), earth_radius=10):
+    def __init__(self, frames=100, name="GoogleEarth", frame_rate=30, earth_center=Vector([0, 0, 0]), earth_radius=10):
         self.frames = frames
         self.name = name
         self.frame_rate = frame_rate
@@ -550,7 +486,7 @@ class GoogleEarthStudio:
 
     def createAnimation(self, path):
         json_data = json.loads(
-            template.replace("{frames}", str(self.frames-1)).replace("{frame_rate}", str(self.frame_rate)).replace(
+            template.replace("{frames}", str(self.frames - 1)).replace("{frame_rate}", str(self.frame_rate)).replace(
                 "{name}", self.name))
         positions = json_data["scenes"][0]["attributes"][0]["attributes"][0]["attributes"][0]["attributes"]
         rotations = json_data["scenes"][0]["attributes"][0]["attributes"][2]["attributes"]
@@ -561,17 +497,24 @@ class GoogleEarthStudio:
         rotations[0]["keyframes"] = self.rotationsX
         rotations[1]["keyframes"] = self.rotationsY
         rotations[2]["keyframes"] = self.rotationsZ
+        print(f"#inserted frames of metric {self.name}: {len(self.rotationsX)}")
 
         with open(path, "w", encoding='utf-8') as f:
             json.dump(json_data, f, ensure_ascii=False)
 
     def append_frame(self, cam, frame):
         long, lat, dist, rotX, rotY, rotZ = extract_data_from_cam(cam, self.earth_center, self.earth_radius)
-        time = frame / self.frames
-        self.longitudes.append({"time": time, "value": get_relative_value(long, -180, 180)})  # -180 bis +180
-        self.latitudes.append({"time": time, "value": get_relative_value(lat, -90, 90)})  # -90 bis +90
+        time = frame/(self.frames-1) # time = np.linspace(0, 1, self.frames)[frame]
+        # self.longitudes.append({"time": time, "value": get_relative_value(long, -180, 180)})  # -180 bis +180
+        self.longitudes.append({"time": time, "value": get_relative_value(long, -math.pi,
+                                                                          math.pi)})  # -180, 179)}) # -math.pi, math.pi)})  # -180 bis +180
+        # self.latitudes.append({"time": time, "value": get_relative_value(lat, -90, 90)})  # -90 bis +90
+        self.latitudes.append(
+            {"time": time, "value": get_relative_value(lat, -math.pi / 2, math.pi / 2)})  # -90,89)}) #   # -90 bis +90
         self.distances.append({"time": time, "value": get_relative_value(dist, -500, 65117481)})  # -500 bis 65117481
-        self.rotationsX.append({"time": time, "value": get_relative_value(rotX, 0,
-                                                                          2 * math.pi)})  # ??? -359,9° bis -359,9° ?? vlt auch nur 0-359,9°
-        self.rotationsY.append({"time": time, "value": get_relative_value(rotY, 0, math.pi)})  # 0° bis 180° || 0 - PI
-        self.rotationsZ.append({"time": time, "value": get_relative_value(rotZ, 0, 2*math.pi)})  # 0° bis 180° || 0 - PI
+        self.rotationsX.append(
+            {"time": time, "value": get_relative_value(rotX, 0, 2 * math.pi)})  # 0, 359)}) #  # ??? 0 - 360°
+        self.rotationsY.append(
+            {"time": time, "value": get_relative_value(rotY, 0, math.pi)})  # 0,179)}) #   # 0° bis 180° || 0 - PI
+        self.rotationsZ.append(
+            {"time": time, "value": get_relative_value(rotZ, 0, 2 * math.pi)})  # 0,359)}) #   # 0° bis 360°