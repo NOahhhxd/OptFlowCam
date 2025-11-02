cea637a2a9513e59ceeede389eb69a89ec84d353
NOahhhxd <nonoapfel041102@gmail.com>
2025-11-02 15:25:37 +0100

    updated calculation of up-vector

--- src/optFlowCam/operators/compare_cams.py
+++ src/optFlowCam/operators/compare_cams.py
@@ -66,43 +66,6 @@ def get_shortest_bb_diagonal(bounding_box, matrix):
     return min_length
 
 
-def create_cam_2(vertex, matrix, focal, scale_base, min_scale, max_scale):
-    dist = scale_base * ((max_scale - min_scale) * random() + min_scale)
-    direction = -vertex.normal
-    look_at_position = matrix @ vertex.co
-    position = look_at_position - direction * dist * focal
-    if direction[1] < 0:
-        return create_cam(position, direction, (0, direction[2], -direction[1]), dist, focal)
-    else:
-        return create_cam(position, direction, (0, -direction[2], direction[1]), dist, focal)
-
-
-def create_cam_from_face(face, matrix, focal, scale_base, min_scale, max_scale, vertices, other_face):
-    """
-    u = random()
-    v = random() * (1 - u)
-    z = 1 - u - v
-    """
-    u, v, w = Vector(random(), random(), random()).normalized()
-
-    other_coords = [matrix @ vertices[idx].co for idx in other_face.vertices]
-    vert_coords = [vertices[idx].co for idx in face.vertices]
-
-    look_at_position = w * other_coords[0] + v * other_coords[1] + u * other_coords[2]
-
-    dist = scale_base * ((max_scale - min_scale) * random() + min_scale)
-    position = matrix @ (vert_coords[0] + dist * focal * face.normal)
-    direction = look_at_position - position
-    direction_n = direction.normalized()
-    scale = direction.length / focal
-    if direction[1] < 0:
-        return create_cam(position, direction_n, Vector([0, direction_n[2], -direction_n[1]]).normalized(), scale,
-                          focal)
-    else:
-        return create_cam(position, direction_n, Vector([0, -direction_n[2], direction_n[1]]).normalized(), scale,
-                          focal)
-
-
 def add_material(objects, color, transparent=False):
     mat = bpy.data.materials.new(name="TransparentMaterial")
     if transparent:
@@ -128,10 +91,12 @@ def add_material(objects, color, transparent=False):
         obj.data.materials.append(mat)
     return mat
 
-
-def random_pos_squared2(vertex_co, vertex_normal, matrix, r):
+def random_spherical_pos(vertex_co, vertex_normal, matrix, r):
+    """
     x = random()
     theta = (x ** 2 + x ** (1 / 2)) / 2 * radians(80)
+    """
+    theta = random() * radians(80)
     phi = random() * 2 * pi
     x = r * sin(theta) * cos(phi)
     y = r * sin(theta) * sin(phi)
@@ -141,13 +106,15 @@ def random_pos_squared2(vertex_co, vertex_normal, matrix, r):
     cam_pos = matrix @ vertex_co + rotation_vec
     return cam_pos, -rotation_vec
 
+
 def get_up(view):
     return view.cross([0, 0, 1]).cross(view)
+
 
 def create_cams(object_mesh, matrix, min_scale, max_scale, is_earth_cam=False):
     scale_base = get_shortest_bb_diagonal(object_mesh.bound_box, object_mesh.matrix_world) / 2
     if is_earth_cam:
-        # 20° FOV of google earth
+        # for 20° FOV of google earth
         focal = 1.639344262295082
     else:
         # 39.6° = standard FOV => 50.0 mm focal length / 36.0 mm sensor width
@@ -156,7 +123,7 @@ def create_cams(object_mesh, matrix, min_scale, max_scale, is_earth_cam=False):
     faces = object_mesh.data.polygons
     face_idx = list(range(len(faces)))
     shuffle(face_idx)
-    # shuffle(face_idx)
+
     cams = []
     for i in range(2):
         face = faces[face_idx[i]]
@@ -166,41 +133,17 @@ def create_cams(object_mesh, matrix, min_scale, max_scale, is_earth_cam=False):
         lookat_pos = u * face_vertices[0].co + v * face_vertices[1].co + w * face_vertices[2].co
         lookat_normal = u * face_vertices[0].normal + v * face_vertices[1].normal + w * face_vertices[2].normal
         dist = scale_base * ((max_scale - min_scale) * random() + min_scale)
-        pos, view = random_pos_squared2(lookat_pos, lookat_normal, matrix, dist)
+        pos,view =  random_spherical_pos(lookat_pos, lookat_normal, matrix, dist)
         view_n = view.normalized()
         scale = view.length / focal
+        cams.append(create_cam(pos, view_n, get_up(view_n).normalized(), scale, focal))
+        """
         if view_n[1] < 0:
             cams.append(create_cam(pos, view_n, Vector([0, view_n[2], -view_n[1]]).normalized(), scale, focal))
         else:
             cams.append(create_cam(pos, view_n, Vector([0, -view_n[2], view_n[1]]).normalized(), scale, focal))
+        """
     return cams
-    # start_idx = vertices_idx[0]
-    """
-    start_idx = randint(0, len(faces) - 1)  # randint(0, len(vertices) - 1)
-
-    while (end_idx := randint(0, len(faces) - 1)) == start_idx:
-        # while (end_idx := randint(0, len(vertices) - 1)) == start_idx:
-        pass
-
-    # end_idx = vertices_idx[-1]
-    start_face = faces[start_idx]  # start_vert = vertices[start_idx]
-    end_face = faces[end_idx]  # end_vert = vertices[end_idx]
-    """
-
-    start_face = faces[face_idx[0]]
-    i = 1
-    while start_face.normal.angle(faces[face_idx[i]].normal) > pi / 4:
-        i += 1
-    start_cam = create_cam_from_face(start_face, matrix, focal, scale_base, min_scale, max_scale, vertices,
-                                     faces[face_idx[i]])
-    end_face = faces[face_idx[-1]]
-    i = -2
-    while end_face.normal.angle(faces[face_idx[i]].normal) > pi / 4:
-        i -= 1
-    end_cam = create_cam_from_face(end_face, matrix, focal, scale_base, min_scale, max_scale, vertices,
-                                   faces[face_idx[i]])
-
-    return start_cam, end_cam
 
 
 def is_triangle_mesh(mesh):
@@ -217,16 +160,15 @@ def is_triangle_mesh(mesh):
 
 
 def get_look_at_points(cams):
-    # pos = lookat - scale * focal * view => looakt = pos + scale*focal*view
+    # pos = lookat - scale * focal * view => lookat = pos + scale*focal*view
     context = bpy.context
     vl = context.view_layer
     scene = context.scene
-    # hit, loc, norm_0, face_idx, obj_0, mw_0 = scene.ray_cast(vl.depsgraph, start, direction)
+
     return [scene.ray_cast(vl.depsgraph, Vector(cam["position"]), Vector(cam["view"]))[1] for cam in cams]
-    # return [Vector(cam["position"]) + cam["frustum_scale"] * cam["focal"] * Vector(cam["view"]) for cam in cams]
 
 
-def create_spheres_at(positions, collection_name, size=Vector((1,1,1))):
+def create_spheres_at(positions, collection_name, size=Vector((1, 1, 1))):
     spheres = []
     for position in positions:
         bpy.ops.mesh.primitive_uv_sphere_add(radius=.2, enter_editmode=False, align='WORLD', location=position,
@@ -253,27 +195,11 @@ def create_outside_cam(start_pos, end_pos, obj, collection_name):
     obj_mid = middleOfBB(obj)
     obj_mid_to_middle = middle - obj_mid
     middle_normal = Vector([obj_mid_to_middle[0], obj_mid_to_middle[1], 0]).normalized()
-    """
-    middle_normal = (end_pos - start_pos).normalized()
-    middle_normal = Vector([middle_normal[1], -middle_normal[0], 0]).normalized()
-
-    # if normal goes into object if dist to mid point > 0 (then inverse direction)
-    
-    if (obj_mid - middle) @ middle_normal > 0:
-        middle_normal *= -1
-    """
     dist = get_shortest_bb_diagonal(obj.bound_box, obj.matrix_world)
     cam_position = middle + 2.5 * dist * middle_normal
     # Ausrichtung auf Mitte des Objekts
     cam_position[2] = obj_mid[2]
     cam_direction = (obj_mid - cam_position).normalized()
-    # cam_direction = (middle - cam_position).normalized()
-    """
-    if cam_direction[1] < 0:
-        up_vec = Vector([0,cam_direction[2], -cam_direction[1]]).normalized()
-    else:
-        up_vec = Vector([0, -cam_direction[2], cam_direction[1]]).normalized()
-    """
     # solange es auf den Mittelpunkt auf gleich Höhe gerichtet ist, kann up-Vector einfach nach oben gerichtet sein
     up_vec = Vector([0, 0, 1])
     cam = create_cam(cam_position, cam_direction, up_vec, 1, 1.3888888888888888)
@@ -316,18 +242,7 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        props = context.scene.OFC.init_props
-
-        ## Uncomment for debug purposes if the operator crashed
-        ## and cannot be started from UI anymore
         return True
-
-        if (len(props.keyframes) >= 2 and
-                all([k.cam != None for k in props.keyframes]) and
-                not context.scene.OFC.op_props.operator_running):
-            return True
-
-        return False
 
     # used to initialize the operator from the context at the moment the operator
     # is called. invoke() is typically used to assign properties which are
@@ -364,7 +279,6 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
             self.report({'ERROR_INVALID_INPUT'}, "min_scale must be smaller than max_scale")
             return {'CANCELLED'}
 
-        # Preparation
         coll = bpy.data.collections.new(f"compare_cams")
         bpy.context.scene.collection.children.link(coll)
         random_coll_name = coll.name
@@ -377,7 +291,7 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
         for metric in metrics:
             cam = add_camera_object(random_coll_name, camera_name=f"compare_cam_{metric}")
             if generate_earth_file:
-                # FOV = 20°
+                # for FOV = 20° in Google Earth
                 f = 1.639344262295082
                 cam.data.lens = f * cam.data.sensor_width
             cams.append(cam)
@@ -397,18 +311,14 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
         if not is_triangle_mesh(mesh):
             self.report({'ERROR_INVALID_INPUT'}, "Selected object be a triangle mesh")
             return {'CANCELLED'}
-        """ convex hull part no longer needed
-        move_around_object.hide_set(True)
-        convex_hull_obj = create_convex_hull_object(move_around_object, random_coll_name)
-        convex_hull_obj.hide_render = True
-        start_cam, end_cam = create_cams(convex_hull_obj, convex_hull_obj.matrix_world, min_scale, max_scale)
-        """
+
         start_cam, end_cam = None, None
         n_frames = 100
         knots = None
 
         if random_cams:
-            start_cam, end_cam = create_cams(move_around_object, move_around_object.matrix_world, min_scale, max_scale, is_earth_cam=generate_earth_file)
+            start_cam, end_cam = create_cams(move_around_object, move_around_object.matrix_world, min_scale, max_scale,
+                                             is_earth_cam=generate_earth_file)
             n_frames = props.n_frames
             knots = [0, n_frames - 1]
         else:
@@ -420,7 +330,7 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
             frame_start = keyframes[0].frame
             frame_end = keyframes[-1].frame
 
-            n_frames = (frame_end - frame_start)+1
+            n_frames = (frame_end - frame_start) + 1
 
             start_cam, end_cam = [cam_to_sample(k.cam) for k in keyframes]
             knots = [k.frame for k in keyframes]
@@ -431,10 +341,6 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
         wm.progress_update(3)
 
         for i, metric in enumerate(metrics):
-            """
-            convex_hull_obj.hide_set(False)
-            move_around_object.hide_set(True)
-            """
             print(metric)
             try:
                 self._path = interpolate_keyframes(cam_samples, knots, cam_samples[0]["focal"],
@@ -453,43 +359,33 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
 
             cam_path_obj = paths[i]
             update_path(cam_path_obj, self._path)
-
             animate_camera(self._path, cams[i])
 
             if render_animation:
-                """
-                convex_hull_obj.hide_set(True)
-                move_around_object.hide_set(False)
-                """
                 filenames.append(f"{exporting_path}\\{metric}.mp4")
                 render_scene(cams[i], filenames[-1], end_frame=n_frames)
             wm.progress_update(3 + 90 / len(metrics) * (i + 1))
 
         if render_animation:
             start_pos, end_pos = get_look_at_points([start_cam, end_cam])
-            start_sphere, end_sphere = create_spheres_at([start_pos, end_pos], random_coll_name,Vector((0.75,0.75,0.75)))
+            start_sphere, end_sphere = create_spheres_at([start_pos, end_pos], random_coll_name,
+                                                         Vector((0.75, 0.75, 0.75)))
             start_cam_sphere, end_cam_sphere = create_spheres_at([start_cam["position"], end_cam["position"]],
-                                                                 random_coll_name, Vector((0.6,0.6,0.6)))
+                                                                 random_coll_name, Vector((0.6, 0.6, 0.6)))
             # copy state before
             mat_copy = move_around_object.data.materials[:]
-            # make object
             added_materials = [add_material([move_around_object], (1, 1, 1, 0.6), False),
-                               # mark path
-                               # add_material([geodesic_path], (1, 0.46, 0, 1), False),
                                # mark start and end point (colored)
                                add_material([start_sphere, end_sphere], (1, 0, 0, 1), False),
                                add_material([start_cam_sphere, end_cam_sphere], (0, 0, 1, 1), False)]
-            # geodesic_path.data.bevel_depth = 0.05
-            # create and positioning cam
             cam = create_outside_cam(start_pos, end_pos, move_around_object, random_coll_name)
-            # make photo
             image_path = f"{exporting_path}\\overview.png"
             render_single_image(cam, image_path)
+
             # undo everything
             move_around_object.data.materials.clear()
             for mat in mat_copy:
                 move_around_object.data.materials.append(mat)
-            # geodesic_path.data.bevel_depth = 0
             for mat in added_materials:
                 bpy.data.materials.remove(mat)
 
@@ -500,11 +396,6 @@ class OFC_OT_CompareInterpolateCamera(bpy.types.Operator):
             bpy.data.objects.remove(start_cam_sphere)
 
             combine_clips(filenames, image_path, exporting_path)
-
-        """
-        if len(filenames)>1:
-            combineClips(exporting_path, filenames)
-        """
 
         wm.progress_end()
         return {'FINISHED'}
